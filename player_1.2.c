6aff2492d751f40190bb83168278832357e0b89e
vikas goyal <35465321+vikasgooyal@users.noreply.github.com>
2018-01-21 20:11:53 +0530

    Update player_1.2.c

--- player_1.2.c
+++ player_1.2.c
@@ -3,19 +3,15 @@ int main()
 {
 	
    long long int n,i,s,j,a=1;
-   
    scanf("%lld",&n);
-   
-   
-   
+   if(n <0 || n >20)
+   printf("0");
+   else{
    for(i=1 ; i<=n ; i++)
    {
    	a=a*i;
    }
-   
-   printf("%lld",a);  
+
+   printf("%lld",a); 
+}
    }
-
-
-
-	
